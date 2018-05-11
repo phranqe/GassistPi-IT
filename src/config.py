aafe50b501b48b9c4229ea7ddb38c0060fe3f2b5
SUPERGIU <supergiu@outlook.com>
2018-05-12 00:57:05 +0200

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+api = {'subscribe_key': '',
+         'publish_key': ''}
+
+ip = '192.168.1.7'
+
+google_cloud_api_key= ''
+
+google_user = ''
+google_psw = ''
+
+parcelApi = ''
+
+pushbulletKey = ''
+
+#Per le ricette --> #https://developer.edamam.com/edamam-recipe-api
+edamamAppId = ''
+edamamAppKey = ''
